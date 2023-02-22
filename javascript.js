5c54301f4a2e8b2e0694787a8258107b6f2f6986
Daniel Tan <122583737+idiotsandwich714@users.noreply.github.com>
2023-02-21 16:22:08 -0800

    Change list items to buttons on index.html

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, num2, operator) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
+
+console.log(operate(5, 5, '+'));
