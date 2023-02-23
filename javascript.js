652fa5f13fd6ef12427caa9d4537bd81eeac013e
idiotsandwich714 <122583737+idiotsandwich714@users.noreply.github.com>
2023-02-22 16:53:42 -0800

    Replace button classes with data types for numbers and operators and implement button behaviors

--- javascript.js
+++ javascript.js
@@ -26,7 +26,39 @@ function operate(num1, num2, operator) {
     }
 }
 
-const keys = document.querySelector('.buttons');
+
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.buttons');
+const tape = calculator.querySelector('.screen');
+
 keys.addEventListener('click', event => {
-    console.log(event.target);
+    if (!event.target.closest('button')) return
+    if (event.target.closest('.clear')) return
+
+    const key = event.target;
+    const keyValue = key.textContent;
+    const type = key.dataset.type;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    if (key.classList.contains('empty')) {
+        tape.textContent = 'UwU';
+    }
+
+    if (type === 'number') {
+        if (tape.textContent === '0') {
+            tape.textContent = keyValue;
+        } else if (previousKeyType === 'operator') {
+            tape.textContent = keyValue;
+        } else {
+            tape.textContent += keyValue;
+        }
+    } 
+
+    calculator.dataset.previousKeyType = type;
+})
+
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click', () => {
+    tape.textContent = '0';
 })
